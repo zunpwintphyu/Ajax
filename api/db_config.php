b7a0ec115b27ec4dd3d0e01697e9140e5144aa44
zunpwintphyu <47861883+zunpwintphyu@users.noreply.github.com>
2019-07-04 16:08:52 +0630

    first commit

---
+++ api/db_config.php
@@ -0,0 +1,7 @@
+<?php
+	define ('DB_USER', "root");
+	define ('DB_PASSWORD', "");
+	define ('DB_DATABASE', "ajax1");
+	define ('DB_HOST', "localhost");
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+?>
